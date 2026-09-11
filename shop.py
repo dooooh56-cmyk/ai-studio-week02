f79d087aa881fe6188a934f991cb9c79dc791c8b
Kim Dohyeok <dooooh56@gmail.com>
2026-09-11 14:09:25 +0900

    feat: modify point rate

--- shop.py
+++ shop.py
@@ -6,7 +6,7 @@ class Customer:
 
   def add_points(self, amount):
     """구매 금액의 5%를 포인트로 적립한다."""
-    self.points += int(amount * 0.05)
+    self.points += int(amount * 0.5)
 
   def get_discount_rate(self):
     """vip는 0.10, basic은 0.03 할인"""
